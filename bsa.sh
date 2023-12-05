46a63db2be58015b87054c1006503039be202324
morganvenable <venable@gmail.com>
2023-12-05 09:36:26 -0800

    updated to most modern Vial layout

---
+++ bsa.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+make svalboard/trackpoint/left:vial
+make svalboard/trackpoint/right:vial
+make svalboard/trackball/left:vial
+make svalboard/trackball/right:vial
+make svalboard/left:vial
+make svalboard/right:vial
