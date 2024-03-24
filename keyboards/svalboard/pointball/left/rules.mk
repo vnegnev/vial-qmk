47aac7ab2dca5bdd352b22a2d5eef2740cc61fff
Morgan Venable <venable@gmail.com>
2024-03-24 11:10:57 -0700

    pointball folder created

---
+++ keyboards/svalboard/pointball/left/rules.mk
@@ -0,0 +1,12 @@
+OPT_DEFS += -DINIT_EE_HANDS_LEFT
+
+
+PS2_MOUSE_ENABLE = yes
+# # this turns on Manna-Harbour's automousekeys:
+MH_AUTO_BUTTONS = yes
+# # this selects trackball driver
+
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = custom
+# this selects trackball driver
+#POINTING_DEVICE_DRIVER = pmw3360
