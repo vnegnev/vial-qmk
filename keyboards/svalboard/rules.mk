dc13cc8ff7ee91381a928217b4865a14dfcefb6f
morganvenable <venable@gmail.com>
2024-06-08 14:12:13 -0700

    A3389 (#52)
    * PMW3360 and PMW3389 both first class citizens
    
    * first CI/CD test
    
    * fixed rules.mk

--- keyboards/svalboard/rules.mk
+++ keyboards/svalboard/rules.mk
@@ -13,28 +13,11 @@ CUSTOM_MATRIX = lite
 SRC += matrix.c $(EXTRA_SRC)
 
 SERIAL_DRIVER = vendor
-# this turns on trackpoint -- dupe in info.json?
-# PS2_MOUSE_ENABLE = yes
-# this turns on trackballs including pimoroni
+
 POINTING_DEVICE_ENABLE = yes
-# this selects trackball driver
 POINTING_DEVICE_DRIVER = custom
-# pimoroni_trackball
-# pmw3389
 
 REPEAT_KEY_ENABLE = yes
-
-ifeq ($(strip $(POINTING_DEVICE_DRIVER)), pimoroni_trackball)
-	OPT_DEFS += -DPOINTING_DEVICE_IS_PIMORONI
-endif
-
-ifeq ($(strip $(POINTING_DEVICE_DRIVER)), pmw3360)
-	OPT_DEFS += -DPOINTING_DEVICE_IS_PMW3360
-endif
-
-ifeq ($(strip $(POINTING_DEVICE_DRIVER)), pmw3389)
-	OPT_DEFS += -DPOINTING_DEVICE_IS_PMW3389
-endif
 
 # this turns on Manna-Harbour's automousekeys:
 MH_AUTO_BUTTONS = yes
---
+++ keyboards/svalboard/trackball/pmw3389/rules.mk
@@ -0,0 +1,6 @@
+# this turns on trackballs including pimoroni
+POINTING_DEVICE_ENABLE = yes
+# this selects trackball driver - moved to left/right for 3360 default, and 3389 subfolders for 3389
+POINTING_DEVICE_DRIVER = pmw3389   
+OPT_DEFS += -DPOINTING_DEVICE_IS_PMW3389
+MH_AUTO_BUTTONS = yes
